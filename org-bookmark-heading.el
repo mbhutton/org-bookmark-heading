c7c7dcc52d3ad78084ea15e95bd7f6037bf02543
Adam Porter <adam@alphapapa.net>
2024-06-22 04:46:20 -0500

    Release: v1.3.2

--- org-bookmark-heading.el
+++ org-bookmark-heading.el
@@ -1,7 +1,7 @@
 ;;; org-bookmark-heading.el --- Emacs bookmark support for Org mode  -*- lexical-binding: t; -*-
 
 ;; Author: Adam Porter <adam@alphapapa.net>
-;; Version: 1.3.2-pre
+;; Version: 1.3.2
 ;; Url: http://github.com/alphapapa/org-bookmark-heading
 ;; Package-Requires: ((emacs "25.1") (compat "29.1.4.5"))
 ;; Keywords: hypermedia, outlines
